7b6ab68472b01b41806b9522a897c67836a20359
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-09-23 21:31:31 +0900

    feat: add Card component

---
+++ src/components/common/Card/index.tsx
@@ -0,0 +1,23 @@
+import palettes from '@/assets/styles/global/pallets';
+import styled from '@emotion/styled';
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  borderRadius?: string;
+  children: ReactNode;
+}
+
+export default function Card({ borderRadius = '12px', children, ...rest }: Props) {
+  return (
+    <CardContainer borderRadius={borderRadius} {...rest}>
+      {children}
+    </CardContainer>
+  );
+}
+
+const CardContainer = styled.div<{ borderRadius: string }>`
+  display: inline-block;
+  border: 1px solid ${palettes.borderGray};
+  box-shadow: 0px 12px 32px 0px rgba(24, 25, 28, 0.08);
+  border-radius: ${({ borderRadius }) => borderRadius};
+`;
