0f21eeaad125ea84b9bd819c6cb4e32b5bd7e15c
YIMSEBIN <jij09123@gmail.com>
2024-09-25 22:33:34 +0900

    feat: add Button component

--- src/components/common/Button/index.stories.tsx
+++ src/components/common/Button/index.stories.tsx
@@ -1,14 +1,20 @@
-import { Meta, StoryObj } from '@storybook/react';
-import Button from '.';
+import type { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof Button> = {
+import Button from './index';
+
+const meta = {
   title: 'common/Button',
-  component: Button,
   tags: ['autodocs'],
-};
+  args: {
+    children: '버튼',
+    theme: 'default',
+  },
+  render: (props) => <Button {...props} />,
+} satisfies Meta<typeof Button>;
 
 export default meta;
+type Story = StoryObj<typeof meta>;
 
-type Story = StoryObj<typeof Button>;
-
-export const Primary: Story = {};
+export const Default: Story = {};
+export const Outlined: Story = { args: { theme: 'outlined' } };
+export const TextButton: Story = { args: { theme: 'textbutton' } };
