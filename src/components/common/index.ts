f1d87abf1fe3bb07bd7b1beb34f4bc4e83048113
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-09-30 20:30:53 +0900

    refactor: add barrel export pattern for common components

---
+++ src/components/common/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './Button';
+export { default as Card } from './Card';
+export { default as Flex } from './Flex';
+export { default as InnerContainer } from './InnerContainer';
+export { default as Input } from './Input';
+export { default as Modal } from './Modal';
