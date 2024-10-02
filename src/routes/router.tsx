2522ac676e013c2c90819cf100586b7c0dd99ce4
tteokbokki-master <0jin.git@gmail.com>
2024-10-02 23:01:19 +0900

    feat: Recruit path 설정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ROUTE_PATH } from './path';
 import App from '@/App';
 import SignIn from '@/pages/auth/SignIn';
+import Recruit from '@/pages/recruit';
 
 export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
     element: <App />,
-    children: [{ path: ROUTE_PATH.SIGN_IN, element: <SignIn /> }],
+    children: [
+      { path: ROUTE_PATH.SIGN_IN, element: <SignIn /> },
+      { path: ROUTE_PATH.Recruit, element: <Recruit /> },
+    ],
   },
 ]);
