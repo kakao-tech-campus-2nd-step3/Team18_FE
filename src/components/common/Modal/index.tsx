80e17708e6544c2f81e94705eba94acb83d9343f
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-10-02 18:12:33 +0900

    feat: add onClose prop to Modal

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -5,26 +5,41 @@ export type Props = {
   textChildren: ReactNode;
   buttonChildren: ReactNode;
   borderRadius?: string;
+  onClose: () => void;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const Modal = ({ textChildren, buttonChildren, borderRadius = '12px', ...props }: Props) => {
+const Modal = ({ textChildren, buttonChildren, borderRadius = '12px', onClose, ...props }: Props) => {
   return (
-    <Card borderRadius={borderRadius} {...props}>
-      <Wrapper>
-        <TextWrapper>{textChildren}</TextWrapper>
-        <ButtonWrapper>{buttonChildren}</ButtonWrapper>
-      </Wrapper>
-    </Card>
+    <Overlay onClick={onClose}>
+      <Card borderRadius={borderRadius} {...props} onClick={(e) => e.stopPropagation()}>
+        <Wrapper>
+          <TextWrapper>{textChildren}</TextWrapper>
+          <ButtonWrapper>{buttonChildren}</ButtonWrapper>
+        </Wrapper>
+      </Card>
+    </Overlay>
   );
 };
 
-const Card = styled.div<{ borderRadius: string }>`
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   width: 100%;
   height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+`;
+
+const Card = styled.div<{ borderRadius: string }>`
   display: flex;
   justify-content: center;
   align-items: center;
   background-color: #fff;
+  border-radius: ${({ borderRadius }) => borderRadius};
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 `;
 
@@ -37,7 +52,7 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const TextWrapper = styled.p`
+const TextWrapper = styled.div`
   width: 100%;
   font-size: 16px;
   font-weight: bold;
