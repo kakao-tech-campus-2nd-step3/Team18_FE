f8fe0ba362e871b7bbfc23c3c7a03aef18d51b86
KimJi-An <j0012a@naver.com>
2024-10-03 17:20:19 +0900

    feat: VisaRegistration path 설정

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ROUTE_PATH } from './path';
 import App from '@/App';
 import SignIn from '@/pages/auth/SignIn';
+import VisaRegistration from '@/pages/visaRegistration';
 
 export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
     element: <App />,
-    children: [{ path: ROUTE_PATH.SIGN_IN, element: <SignIn /> }],
+    children: [
+      { path: ROUTE_PATH.SIGN_IN, element: <SignIn /> },
+      { path: ROUTE_PATH.VISA_REGISTRATION, element: <VisaRegistration /> },
+    ],
   },
 ]);
