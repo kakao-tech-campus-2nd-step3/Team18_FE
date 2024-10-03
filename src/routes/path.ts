4670654af4b787f6d9b95a5f09b6f9a37cbbc2d8
YIMSEBIN <jij09123@gmail.com>
2024-10-03 21:41:59 +0900

    feat: add path of PostNotice page

--- src/routes/path.ts
+++ src/routes/path.ts
@@ -1,4 +1,5 @@
 export const ROUTE_PATH = {
   HOME: '/',
   SIGN_IN: '/sign-in',
+  POST_NOTICE: '/post-notice',
 };
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,11 +2,15 @@ import { createBrowserRouter } from 'react-router-dom';
 import { ROUTE_PATH } from './path';
 import App from '@/App';
 import SignIn from '@/pages/auth/SignIn';
+import { PostNotice } from '@/pages/employer/postNotice';
 
 export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
     element: <App />,
-    children: [{ path: ROUTE_PATH.SIGN_IN, element: <SignIn /> }],
+    children: [
+      { path: ROUTE_PATH.SIGN_IN, element: <SignIn /> },
+      { path: ROUTE_PATH.POST_NOTICE, element: <PostNotice /> },
+    ],
   },
 ]);
