e9ae37e9701d5c80cc0bbbbc7d4452362bf86e6e
KimJi-An <j0012a@naver.com>
2024-10-09 00:49:04 +0900

    feat: 고용주 마이페이지 내 공고글 섹션 구현

--- src/pages/myPage/employer/index.tsx
+++ src/pages/myPage/employer/index.tsx
@@ -2,26 +2,48 @@ import Layout from '@/features/layout';
 import CompanyLogo from '@/features/myPage/CompanyInfo/coupang.png';
 import CompanyInfo from '@/features/myPage/CompanyInfo';
 import styled from '@emotion/styled';
-import { InnerContainer } from '@/components/common';
+import { Flex, InnerContainer } from '@/components/common';
+import MyRecruitments from '@/features/myPage/MyRecruitments';
 
-const initialData = {
+const initialCompanyData = {
   logo: CompanyLogo,
   company: '쿠팡 유성점',
   industryOccupation: '온라인 소매',
   revenuePerYear: '15조',
 };
 
+const initialRecruitmentsData = [
+  {
+    koreanTitle: '제목1',
+    area: '대전 유성구',
+    koreanDetailedDescription: '쿠팡 유성점에서 아르바이트 모집합니다.',
+  },
+  {
+    koreanTitle: '제목2',
+    area: '대전 유성구',
+    koreanDetailedDescription: '쿠팡 유성점에서 아르바이트 모집합니다.',
+  },
+  {
+    koreanTitle: '제목3',
+    area: '대전 유성구',
+    koreanDetailedDescription: '쿠팡 유성점에서 아르바이트 모집합니다.',
+  },
+];
+
 export default function EmployerMyPage() {
   return (
     <Layout>
       <MainContainer>
         <InnerContainer maxWidth="1300px">
-          <CompanyInfo
-            logo={initialData.logo}
-            company={initialData.company}
-            industryOccupation={initialData.industryOccupation}
-            revenuePerYear={initialData.revenuePerYear}
-          />
+          <Flex direction="column" gap={{ y: '60px' }}>
+            <CompanyInfo
+              logo={initialCompanyData.logo}
+              company={initialCompanyData.company}
+              industryOccupation={initialCompanyData.industryOccupation}
+              revenuePerYear={initialCompanyData.revenuePerYear}
+            />
+            <MyRecruitments recruitmentsList={initialRecruitmentsData} />
+          </Flex>
         </InnerContainer>
       </MainContainer>
     </Layout>
