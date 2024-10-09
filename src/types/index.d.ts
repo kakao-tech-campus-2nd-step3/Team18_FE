7d14d94355dd70ffb50756b293aeb97eeead4855
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-10-09 20:22:29 +0900

    feat: add Home page component

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+export type BannerItem = {
+  id: number;
+  imageUrl;
+};
+
+export type RecruitmentItem = {
+  recruitmentId: number;
+  image: string;
+  koreanTitle: string;
+  vietnameseTitle: string;
+  companyName: string;
+  salary: number;
+  workHours: string;
+  area: string;
+};
