9818e0a4e1df17831ee6492924e98003beae6989
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-10-09 20:12:35 +0900

    fix: change wrap's prop type to boolean

--- src/components/common/Flex/index.tsx
+++ src/components/common/Flex/index.tsx
@@ -10,6 +10,7 @@ type FlexGap = {
 interface FlexProps {
   direction?: 'column' | 'row';
   gap?: FlexGap;
+  wrap?: boolean;
   justifyContent?: CSS.Properties['justifyContent'];
   alignItems?: CSS.Properties['alignItems'];
 }
@@ -19,13 +20,21 @@ type Props = HTMLAttributes<HTMLDivElement> & FlexProps;
 export default function Flex({
   direction = 'row',
   gap,
+  wrap = false,
   justifyContent = 'start',
   alignItems = 'start',
   children,
   ...rest
 }: Props) {
   return (
-    <Container direction={direction} gap={gap} justifyContent={justifyContent} alignItems={alignItems} {...rest}>
+    <Container
+      direction={direction}
+      gap={gap}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+      wrap={wrap}
+      {...rest}
+    >
       {children}
     </Container>
   );
@@ -34,6 +43,7 @@ export default function Flex({
 const Container = styled.div<FlexProps>`
   width: 100%;
   display: flex;
+  flex-wrap: ${({ wrap }) => (wrap ? 'wrap' : 'nowrap')};
   flex-direction: ${(p) => p.direction};
   justify-content: ${(p) => p.justifyContent};
   align-items: ${(p) => p.alignItems};
