17c9bd072f12d448be1d6b7e7ada272fe97c9669
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-10-09 19:57:44 +0900

    feat: add path home

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,18 +5,20 @@ import App from '@/App';
 import Recruit from '@/pages/recruit';
 import VisaRegistration from '@/pages/visaRegistration';
 import { PostNotice } from '@/pages/employer/postNotice';
+import Home from '@/pages/home';
 
 export const router = createBrowserRouter([
   {
     path: ROUTE_PATH.HOME,
     element: <App />,
     children: [
+      { path: ROUTE_PATH.HOME, element: <Home /> },
       { path: ROUTE_PATH.AUTH.SIGN_IN, element: <SignIn /> },
       {
         path: ROUTE_PATH.AUTH.SIGN_UP,
         element: <SignUp />,
       },
-      { path: ROUTE_PATH.Recruit, element: <Recruit /> },
+      { path: ROUTE_PATH.RECRUIT, element: <Recruit /> },
       { path: ROUTE_PATH.VISA_REGISTRATION, element: <VisaRegistration /> },
       { path: ROUTE_PATH.POST_NOTICE, element: <PostNotice /> },
     ],
