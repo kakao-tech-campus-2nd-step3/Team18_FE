4b0ad0ca6b81d1298c9484efe801997877b58052
YIMSEBIN <jij09123@o.cnu.ac.kr>
2024-10-10 13:20:16 +0900

    fix: 헤더+푸터 -> 레이아웃으로 통합

--- src/pages/employer/postNotice/index.tsx
+++ src/pages/employer/postNotice/index.tsx
@@ -1,69 +1,68 @@
 import { Button, Flex, Input, Typo } from '@/components/common';
-import Footer from '@/features/layout/footer';
-import Header from '@/features/layout/Header';
+import Layout from '@/features/layout';
 import styled from '@emotion/styled';
 
 export const PostNotice = () => {
   return (
     <>
-      <Header />
-      <section>
-        <Flex direction="column" alignItems="center">
-          <LineWrapper>
-            <Flex direction="column" justifyContent="space-between" alignItems="center" style={{ width: '100%' }}>
-              <Typo element="h1" size="20px" style={{ marginBottom: '24px' }}>
-                구인 글 업로드
-              </Typo>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  급여
+      <Layout>
+        <section>
+          <Flex direction="column" alignItems="center">
+            <LineWrapper>
+              <Flex direction="column" justifyContent="space-between" alignItems="center" style={{ width: '100%' }}>
+                <Typo element="h1" size="20px" style={{ marginBottom: '24px' }}>
+                  구인 글 업로드
                 </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  근무기간
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  근무요일
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  근무시간
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  고용형태
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  비자조건
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <InputContainer>
-                <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
-                  우대사항
-                </Typo>
-                <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-              </InputContainer>
-              <Button theme="default" style={{ marginTop: '52px' }}>
-                등록하기
-              </Button>
-            </Flex>
-          </LineWrapper>
-        </Flex>
-      </section>
-      <Footer />
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    급여
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    근무기간
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    근무요일
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    근무시간
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    고용형태
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    비자조건
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <InputContainer>
+                  <Typo element="p" size="16px" style={{ fontWeight: 'bold' }}>
+                    우대사항
+                  </Typo>
+                  <Input style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+                </InputContainer>
+                <Button theme="default" style={{ marginTop: '52px' }}>
+                  등록하기
+                </Button>
+              </Flex>
+            </LineWrapper>
+          </Flex>
+        </section>
+      </Layout>
     </>
   );
 };
