05ef4df963fa26f697d2896cf3699249db56f11e
tteokbokki-master <0jin.git@gmail.com>
2024-10-10 12:30:08 +0900

    feat: 지원서 작성 페이지 구현 - react-hook-form을 사용해서 구현 했습니다. - 유효성 검사를 통해 모든 값을 필수로 입력받게 했습니다.

---
+++ src/pages/apply/applypage/useApplyHook.tsx
@@ -0,0 +1,44 @@
+import useToggle from '@/hooks/useToggle';
+import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+export interface ApplyInfoProps {
+  name: string;
+  address: string;
+  phoneNumber: string;
+  applyMotivation: string;
+}
+
+export function useApplyHook() {
+  const [isToggle, toggle] = useToggle();
+  const [formData, setFormData] = useState<ApplyInfoProps | null>(null);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ApplyInfoProps>();
+
+  const onSubmit: SubmitHandler<ApplyInfoProps> = (data) => {
+    setFormData(data);
+    toggle();
+  };
+
+  const handleApplySubmit = () => {
+    // 추후 post 요청 여기서 하면 됨!.!
+    if (formData) {
+      console.log(formData);
+      alert('지원 완료!');
+      toggle();
+    }
+  };
+
+  return {
+    isToggle,
+    toggle,
+    register,
+    handleSubmit,
+    onSubmit,
+    handleApplySubmit,
+    errors,
+  };
+}
