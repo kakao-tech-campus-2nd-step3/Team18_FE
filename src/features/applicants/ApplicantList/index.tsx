864c59741109360f63b7b6026b4782604150d21b
KimJi-An <j0012a@naver.com>
2024-10-16 01:15:41 +0900

    refactor: ApplicantList 테이블 컴포넌트 분리

--- src/features/applicants/ApplicantList/index.tsx
+++ src/features/applicants/ApplicantList/index.tsx
@@ -1,8 +1,5 @@
-import { palettes } from '@/assets/styles/global/palettes';
-import { Button, Flex, List, Typo } from '@/components/common';
-import { responsiveStyle } from '@/utils/responsive';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
+import { Flex, Typo } from '@/components/common';
+import ApplicantsTable from './ApplicantsTable';
 
 interface ApplicantProps {
   userId: number;
@@ -28,100 +25,7 @@ export default function ApplicantList({ applicantList }: ApplicantListProps) {
           총 {applicantList.length}건
         </Typo>
       </Flex>
-      <Table>
-        <thead>
-          <tr>
-            <Th>이름</Th>
-            <Th>국적</Th>
-            <Th>한국어 실력</Th>
-            <Th></Th>
-          </tr>
-        </thead>
-        <tbody>
-          <List
-            items={applicantList}
-            renderItem={(applicant) => (
-              <tr key={applicant.applyId}>
-                <Td>{applicant.name}</Td>
-                <Td>{applicant.applicantNation}</Td>
-                <Td>{applicant.korean}</Td>
-                <Td css={buttonsCellStyle}>
-                  <Flex justifyContent="flex-end" alignItems="center" gap={{ x: '20px' }} css={buttonGroupStyle}>
-                    <Button css={buttonStyle}>이력서</Button>
-                    <Button css={buttonStyle}>지원서</Button>
-                    <Button css={buttonStyle}>계약하기</Button>
-                  </Flex>
-                </Td>
-              </tr>
-            )}
-          />
-        </tbody>
-      </Table>
+      <ApplicantsTable applicantList={applicantList} />
     </Flex>
   );
 }
-
-const Table = styled.table`
-  ${responsiveStyle({
-    default: {
-      width: '100%',
-      fontSize: '16px',
-      borderCollapse: 'collapse',
-      wordWrap: 'break-word',
-    },
-    tablet: {
-      fontSize: '15px',
-    },
-  })}
-`;
-
-const Th = styled.th`
-  padding: 10px 30px;
-  background-color: ${palettes.backgroundGray};
-  border: none;
-  border-top: 1px solid ${palettes.black};
-  border-bottom: 1px solid ${palettes.black};
-  text-align: left;
-`;
-
-const Td = styled.td`
-  padding: 20px 30px;
-  border: none;
-  border-bottom: 1px solid ${palettes.borderGray};
-  text-align: left;
-`;
-
-const buttonsCellStyle = css`
-  ${responsiveStyle({
-    default: {
-      width: '600px',
-    },
-    tablet: {
-      width: '280px',
-    },
-    mobile: {
-      width: '200px',
-    },
-  })}
-`;
-
-const buttonGroupStyle = css`
-  ${responsiveStyle({
-    tablet: {
-      flexDirection: 'column',
-      alignItems: 'stretch',
-      gap: '10px',
-    },
-  })}
-`;
-
-const buttonStyle = css`
-  ${responsiveStyle({
-    default: {
-      whiteSpace: 'nowrap',
-    },
-    mobile: {
-      fontSize: '15px',
-    },
-  })}
-`;
