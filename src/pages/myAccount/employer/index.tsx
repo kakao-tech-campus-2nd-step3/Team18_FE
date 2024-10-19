83f4b8906fb3d4c031dd068e7c31470f47f5835c
KimJi-An <j0012a@naver.com>
2024-10-19 23:48:03 +0900

    refactor: EmployerMyAccount 페이지에서 mock 데이터 분리 및 코드 정리

--- src/pages/myAccount/employer/index.tsx
+++ src/pages/myAccount/employer/index.tsx
@@ -1,44 +1,21 @@
 import { Flex, InnerContainer, Typo } from '@/components/common';
 import Layout from '@/features/layout';
-import CompanyList from '@/features/myAccount/employer/MyCompanies/CompanyList';
-import styled from '@emotion/styled';
-
-const initialData = [
-  {
-    id: 1,
-    name: '쿠팡 유성점',
-    industryOccupation: '온라인 소매',
-    brand: '쿠팡',
-    revenuePerYear: 1000000,
-    logoImage: 'src/assets/images/coupang.png',
-  },
-  {
-    id: 2,
-    name: '쿠팡 유성점',
-    industryOccupation: '온라인 소매',
-    brand: '쿠팡',
-    revenuePerYear: 1000000,
-    logoImage: 'src/assets/images/coupang.png',
-  },
-];
+import CompanyList from '@/features/companies/CompanyList';
+import { companyList } from './index.mock';
 
 export default function EmployerMyAccount() {
   return (
     <Layout>
-      <MainContainer>
-        <InnerContainer>
+      <div>
+        <InnerContainer css={{ padding: '60px 0 80px 0' }}>
           <Flex direction="column" gap={{ y: '60px' }}>
             <Typo element="h2" size="36px" style={{ fontWeight: 'bold' }}>
               사장님, 안녕하세요!
             </Typo>
-            <CompanyList companyList={initialData} />
+            <CompanyList companyList={companyList} />
           </Flex>
         </InnerContainer>
-      </MainContainer>
+      </div>
     </Layout>
   );
 }
-
-const MainContainer = styled.div`
-  padding: 60px 0;
-`;
