9e1cc130288641e6b1c172bac48787f5b1aa2551
YIMSEBIN <jij09123@gmail.com>
2024-10-24 16:35:59 +0900

    style: Button props 이름 변경

--- src/pages/employer/postNotice/PostNotice.tsx
+++ src/pages/employer/postNotice/PostNotice.tsx
@@ -4,45 +4,43 @@ import styled from '@emotion/styled';
 
 const PostNotice = () => {
   return (
-    <>
-      <Layout>
-        <section>
-          <Flex direction="column" alignItems="center">
-            <LineWrapper>
-              <Flex direction="column" justifyContent="space-between" alignItems="center" style={{ width: '100%' }}>
-                <Typo element="h1" size="20px" style={{ fontWeight: 'bold', marginBottom: '24px' }}>
-                  구인 글 업로드
-                </Typo>
-                <InputContainer>
-                  <Input label="급여" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="근무기간" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="근무요일" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="근무시간" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="고용형태" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="비자조건" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <InputContainer>
-                  <Input label="우대사항" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
-                </InputContainer>
-                <Button theme="default" style={{ marginTop: '52px' }}>
-                  등록하기
-                </Button>
-              </Flex>
-            </LineWrapper>
-          </Flex>
-        </section>
-      </Layout>
-    </>
+    <Layout>
+      <section>
+        <Flex direction="column" alignItems="center">
+          <LineWrapper>
+            <Flex direction="column" justifyContent="space-between" alignItems="center" style={{ width: '100%' }}>
+              <Typo element="h1" size="20px" style={{ fontWeight: 'bold', marginBottom: '24px' }}>
+                구인 글 업로드
+              </Typo>
+              <InputContainer>
+                <Input label="급여" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="근무기간" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="근무요일" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="근무시간" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="고용형태" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="비자조건" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <InputContainer>
+                <Input label="우대사항" style={{ width: '700px', height: '48px', marginTop: '12px' }}></Input>
+              </InputContainer>
+              <Button theme="default" style={{ marginTop: '52px' }}>
+                등록하기
+              </Button>
+            </Flex>
+          </LineWrapper>
+        </Flex>
+      </section>
+    </Layout>
   );
 };
 
