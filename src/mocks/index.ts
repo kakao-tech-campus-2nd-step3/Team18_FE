20847b3bfa07034bd217972b4bbb7f5a73f0a2ce
YIMSEBIN <jij09123@gmail.com>
2024-10-24 19:42:31 +0900

    feat: 근로자 마이페이지 mock 핸들러 추가

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,4 +1,5 @@
+import { EmployeePageMockHandler } from '@/apis/employee/mock/getMyApplication.mock';
 import { noticesMockHandler } from '@/apis/employer/mock/postNotice.mock';
 import { setupWorker } from 'msw/browser';
 
-export const worker = setupWorker(...noticesMockHandler);
+export const worker = setupWorker(...noticesMockHandler, ...EmployeePageMockHandler);
