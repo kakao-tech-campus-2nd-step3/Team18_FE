9ca16b661f37cb4966208088a2f0ae6da0d7964b
tteokbokki-master <0jin.git@gmail.com>
2024-10-28 15:42:51 +0900

    feat: 사인 등록 페이지 api 연동(msw)

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,10 +2,12 @@ import { recruitmentsMockHandler } from '@/apis/home/mocks/recruitmentsMockHandl
 import { slidesMockHandler } from '@/apis/home/mocks/slidesMockHandler';
 import { EmployeePageMockHandler } from '@/apis/employee/mock/getMyApplication.mock';
 import { noticesMockHandler } from '@/apis/employer/mock/postNotice.mock';
+import { registerSignMockHandler } from '@/apis/registerSign/registerSign.mock';
 
 export const handlers = [
   ...recruitmentsMockHandler,
   ...slidesMockHandler,
   ...noticesMockHandler,
   ...EmployeePageMockHandler,
+  ...registerSignMockHandler,
 ];
