1f3e273998107d47d14b72850ddc2790aeafe790
KimJi-An <j0012a@naver.com>
2024-10-31 01:02:23 +0900

    feat: Header 네비게이션 기능 추가
    - 로고 클릭 시 홈 화면으로 이동
    - 로그인 버튼 클릭 시 로그인 페이지로 이동
    - 채용공고 등록 버튼 클릭 시 구인글 업로드 페이지로 이동
    - 프로필 이미지 클릭 시 마이페이지로 이동

--- src/features/layout/Header/index.tsx
+++ src/features/layout/Header/index.tsx
@@ -15,7 +15,7 @@ import {
   Nav,
 } from './index.styles';
 import { UserData } from '@/types';
-import { useEffect, useState } from 'react';
+import { startTransition, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ROUTE_PATH from '@/routes/path';
 
@@ -32,6 +32,16 @@ export default function Header() {
     }
   }, []);
 
+  const handleLogoClick = () => {
+    startTransition(() => {
+      navigate(ROUTE_PATH.HOME);
+    });
+  };
+
+  const handleLogin = () => {
+    navigate(ROUTE_PATH.AUTH.SIGN_IN);
+  };
+
   const handleLogout = () => {
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -39,25 +49,39 @@ export default function Header() {
     navigate(ROUTE_PATH.HOME);
   };
 
+  const handlePostNotice = () => {
+    navigate(ROUTE_PATH.POST_NOTICE);
+  };
+
+  const handleProfileClick = () => {
+    if (user?.type === 'employer') {
+      navigate(ROUTE_PATH.MY_PAGE.EMPLOYER);
+    } else if (user?.type === 'employee') {
+      navigate(ROUTE_PATH.EMPLOYEE.EMPLOYEE_PAGE);
+    }
+  };
+
   return (
     <HeaderContainer>
       <Flex justifyContent="space-between" alignItems="center" css={flexStyle}>
-        <LogoImg />
+        <LogoImg onClick={handleLogoClick} />
         <Flex justifyContent="flex-end" css={menuIconStyle} onClick={toggleMenu}>
           {menuOpen ? <CloseIcon /> : <MenuIcon />}
         </Flex>
         <Nav open={menuOpen}>
           <LanguageFilter />
           {!user ? (
-            <Button style={customButtonStyle}>로그인</Button>
+            <Button style={customButtonStyle} onClick={handleLogin}>
+              로그인
+            </Button>
           ) : (
             <>
               {user.type === 'employer' && (
-                <Button design="outlined" style={commonButtonStyle}>
+                <Button design="outlined" style={commonButtonStyle} onClick={handlePostNotice}>
                   채용공고 등록
                 </Button>
               )}
-              <Flex justifyContent="center" alignItems="center">
+              <Flex justifyContent="center" alignItems="center" onClick={handleProfileClick}>
                 <Image url={user.profileImage} size={{ width: '40px', height: '40px' }} css={imageStyle} />
               </Flex>
               <Button style={customButtonStyle} onClick={handleLogout}>
