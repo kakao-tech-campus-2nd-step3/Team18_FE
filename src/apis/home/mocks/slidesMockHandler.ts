dbdeb04c67b14f146779ae1998d163f8da1dd671
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2024-11-12 19:55:55 +0900

    fix: change slidesMockHandler

--- src/apis/home/mocks/slidesMockHandler.ts
+++ src/apis/home/mocks/slidesMockHandler.ts
@@ -5,15 +5,12 @@ export const slidesMockHandler = [http.get(APIPath.getSlides, () => HttpResponse
 
 const SLIDER_RESPONSE_DATA = [
   {
-    id: 1,
     imageUrl: 'https://www.v-on.kr/wp-content/uploads/2022/06/IT_twi001t2302755-1024x683.jpg',
   },
   {
-    id: 2,
     imageUrl: 'https://www.v-on.kr/wp-content/uploads/2022/06/IT_twi001t2302755-1024x683.jpg',
   },
   {
-    id: 3,
     imageUrl: 'https://www.v-on.kr/wp-content/uploads/2022/06/IT_twi001t2302755-1024x683.jpg',
   },
 ];
